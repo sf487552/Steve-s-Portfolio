38aaa30b10ad506f51f090651b4435b6a8cc9bdd
Steve Fokas <sf487552@gmail.com>
2021-09-25 14:58:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// $(function(){
+//     var link = $("nav a");
+//    //click handler
+//     link.on("click" , function(){
+//       var $this = $(this);
+//       var page = $this.data("page");
+                    
+//       $("body").removeClass().addClass(page);
+//       link.removeClass("active");
+//       $this.addClass("active");
+//     })
+//   });
+
+  // on click function for sections
+
+  // const targetDiv = document.getElementById("about-me");
+  // const btn = document.getElementById("about");
+  // btn.onclick = function () {
+  //   if (targetDiv.style.display !== "none") {
+  //     targetDiv.style.display = "none";s
+  //   } else {
+  //     targetDiv.style.display = "block";
+  //   }
+  // };
+
+  // const targetDiv = document.getElementById("other-work");
+  // const btn = document.getElementById("about");
+  // btn.onclick = function () {
+  //   if (targetDiv.style.display !== "none") {
+  //     targetDiv.style.display = "none";s
+  //   } else {
+  //     targetDiv.style.display = "block";
+  //   }
+  // };
