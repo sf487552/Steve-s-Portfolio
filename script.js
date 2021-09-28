e0acd925dd72ca9f72f5b00dff38c2a5930c5995
Steve Fokas <sf487552@gmail.com>
2021-09-28 18:44:14 -0400

    more styling edits

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ $('nav a').click(function() {
   $('.container').hide();
   $(this.getAttribute('href')).show()
 });
+
+$('.homeBtn').click(function() {
+  $('.container').hide();
+  $(this.getAttribute('href')).show()
+});
+
