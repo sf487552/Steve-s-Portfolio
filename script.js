d757a5be02f9d362e87509b8dfec0ba26b842e1e
Steve Fokas <sf487552@gmail.com>
2021-10-02 10:03:16 -0400

    fixing projects page

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ $('.homeBtn').click(function() {
   $(this.getAttribute('href')).show()
 });
 
+
+$('.button-1').click(function() {
+  $('.container').hide();
+  $(this.getAttribute('href')).show()
+});
+
+$('.button-2').click(function() {
+  $('.container').hide();
+  $(this.getAttribute('href')).show()
+});
+
+$('.button-3').click(function() {
+  $('.container').hide();
+  $(this.getAttribute('href')).show()
+});
+
+
+
